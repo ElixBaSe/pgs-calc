6cdeff1a729b184fe390d2029a11827e03a97317
Elizabeth Barrera <106718288+ElixBaSe@users.noreply.github.com>
2025-11-11 13:13:09 -0600

    Add Nextflow pipeline script for PGS calculation

---
+++ pipeline-pgs-calc.sh
@@ -0,0 +1,12 @@
+# Nextflow
+
+cd ~/PGS_Catalog
+
+  nextflow run pgscatalog/pgsc_calc \
+  -profile docker \
+  --input samplesheet_oriGen50k_pfile.csv \
+  --scorefile Suzuki_T2D_MA_scorefile.txt \
+  --target_build GRCh38 \
+  --outdir results_oriGen50k_Suzuki_MA_pfile \
+  --max_memory '14 GB' \
+  -resume
